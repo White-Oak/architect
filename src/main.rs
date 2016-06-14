68457af47ea06889ca91174bd58b34ac190557d2
White-Oak <lpzhelud@gmail.com>
2016-06-14 12:22:03 +0300

    Added commits to stats and attempted to fix incorrect calculations
    Although, to no avail.

--- src/main.rs
+++ src/main.rs
@@ -8,25 +8,27 @@ mod output;
 use diff::*;
 
 use std::collections::*;
-use ansi_term::Colour::{Green, Red};
+use ansi_term::Colour::{Green, Red, Yellow};
 
 fn main() {
     let mut stats = gather_stats().unwrap();
     let mut gathered: BTreeMap<String, ResultStat> = BTreeMap::new();
     for stat in &mut stats {
         if !gathered.contains_key(&stat.author) {
-            let new_stat = ResultStat::new(stat.author.clone());
+            let mut new_stat = ResultStat::new(stat.author.clone());
             gathered.insert(stat.author.clone(), new_stat);
-        } else {
-            let s = gathered.get_mut(&stat.author).unwrap();
-            s.inserts += stat.inserts;
-            s.dels += stat.dels;
         }
+        let mut s = gathered.get_mut(&stat.author).unwrap();
+        s.inserts += stat.inserts;
+        s.dels += stat.dels;
+        s.commits += 1;
     }
 
     for stat in gathered.values(){
         println!("Statistics for {}", stat.author);
-        println!("Insertions: {}; Deletions: {}", Green.paint(stat.inserts.to_string()),
+        println!("Commits: {}; Insertions: {}; Deletions: {}",
+            Yellow.paint(stat.commits.to_string()),
+            Green.paint(stat.inserts.to_string()),
             Red.paint(stat.dels.to_string()));
         println!("");
     }
