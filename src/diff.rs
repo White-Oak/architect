2ab795beb62a131cff0af3a6c35bdc77ca9ed626
White-Oak <lpzhelud@gmail.com>
2016-06-16 17:43:58 +0300

    Added progress counter

--- src/diff.rs
+++ src/diff.rs
@@ -4,6 +4,8 @@ use std::collections::*;
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::sync::mpsc::channel;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::rc::Rc;
 
 pub fn gather_stats() -> Result<Vec<Stat>, Error> {
     // Open repo on '.'
@@ -40,19 +42,36 @@ pub fn gather_stats() -> Result<Vec<Stat>, Error> {
 
     let mut revwalk = repo.revwalk()?;
     revwalk.push_head()?;
-    let total = revwalk.count() - 1;
-    let mut revwalk = repo.revwalk()?;
-    revwalk.push_head()?;
     let mut commits = Vec::new();
     for commit in revwalk {
         commits.push(commit?);
     }
+    let total = commits.len();
     println!("Total: {}", total);
     print!("0/{}", total);
     stdout().flush().unwrap();
 
     let first = commits.clone().into_iter().skip(total/2);
     let (tx, rx) = channel();
+    let current = AtomicUsize::new(0);
+    let arc_current = Arc::new(current);
+    let local_arc_current = arc_current.clone();
+    thread::spawn(move || {
+        let repo = Repository::open(".").unwrap();
+        let mut stats = Vec::new();
+        for next in first {
+            let commit = repo.find_commit(next).unwrap();
+            for parent in commit.parents() {
+                stats.push(calculate_diff(&repo, &parent, &commit).unwrap());
+            }
+            local_arc_current.fetch_add(1, Ordering::SeqCst);
+        }
+        tx.send(stats).unwrap();
+    });
+
+    let first = commits.clone().into_iter().take(total/2);
+    let (tx2, rx2) = channel();
+    let local_arc_current = arc_current.clone();
     thread::spawn(move || {
         let repo = Repository::open(".").unwrap();
         let mut stats = Vec::new();
@@ -61,21 +80,28 @@ pub fn gather_stats() -> Result<Vec<Stat>, Error> {
             for parent in commit.parents() {
                 stats.push(calculate_diff(&repo, &parent, &commit).unwrap());
             }
+            local_arc_current.fetch_add(1, Ordering::SeqCst);
         }
-        tx.send(stats).unwrap();
+        tx2.send(stats).unwrap();
     });
 
-    let mut revwalk = repo.revwalk().unwrap();
-    revwalk.push_head().unwrap();
-    for (_, next) in commits.into_iter().take(total/2).enumerate() {
-        let commit = repo.find_commit(next).unwrap();
-        for parent in commit.parents() {
-            stats.push(calculate_diff(&repo, &parent, &commit).unwrap());
+    let mut last_percent = 0;
+    loop{
+        let counter = arc_current.load(Ordering::Relaxed);
+        let of_half_percents = counter * 200 / total;
+        if of_half_percents - last_percent >= 1 {
+            print!("\r{}/{}", arc_current.load(Ordering::Relaxed), total);
+            stdout().flush().unwrap();
+            last_percent = of_half_percents;
+        }
+        if counter >= total {
+            break
         }
     }
 
-
     stats.append(&mut rx.recv().unwrap());
+    stats.append(&mut rx2.recv().unwrap());
+    print!("\r{}/{}", total, total);
     println!("");
     Ok(stats)
 }
