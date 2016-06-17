68d213c0c035bfa72e08d6f226f88a60e2034284
White-Oak <lpzhelud@gmail.com>
2016-06-17 13:47:37 +0300

    Changed colors

--- src/view.rs
+++ src/view.rs
@@ -37,9 +37,10 @@ ApplicationWindow {
 
       BarSeries {
           id: mySeries
+          labelsFormat: ""
           axisX: BarCategoryAxis { categories: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" ] }
-          BarSet { label: "Additions"; values: [a1, a2, a3, a4, a5, a6, a7] }
-          BarSet { label: "Deletions"; values: [d1, d2, d3, d4, d5, d6, d7] }
+          BarSet { label: "Additions"; values: [a1, a2, a3, a4, a5, a6, a7]; color: "green" }
+          BarSet { label: "Deletions"; values: [d1, d2, d3, d4, d5, d6, d7]; color: "red" }
       }
   }
 }
