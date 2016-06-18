4878de399923ec8261385b2f596fb2b3f6abd9fb
White-Oak <lpzhelud@gmail.com>
2016-06-18 19:45:39 +0300

    Added skipping of merge commits

--- src/diff.rs
+++ src/diff.rs
@@ -53,8 +53,8 @@ pub fn gather_stats() -> Result<Vec<Stat>, Error> {
     let size = total / threads_num;
     println!("Total: {}", total);
     println!("Counting on {} threads with {} commits per one",
-             threads_num,
-             size);
+    threads_num,
+    size);
     print!("0/{}", total);
     stdout().flush().unwrap();
 
@@ -79,8 +79,12 @@ pub fn gather_stats() -> Result<Vec<Stat>, Error> {
             let mut stats = Vec::new();
             for next in commits {
                 let commit = repo.find_commit(next).unwrap();
-                for parent in commit.parents() {
-                    stats.push(calculate_diff(&repo, &parent, &commit).unwrap());
+                let parents = commit.parents();
+                // Skip if merge commit 
+                if parents.len() == 1 {
+                    for parent in commit.parents() {
+                        stats.push(calculate_diff(&repo, &parent, &commit).unwrap());
+                    }
                 }
                 arc_current.fetch_add(1, Ordering::Relaxed);
             }
