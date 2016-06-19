c5191988a349347115e1e0baac50f7bbc4c5baa1
White-Oak <lpzhelud@gmail.com>
2016-06-19 17:21:58 +0300

    Fixed #3

--- src/view/html.rs
+++ src/view/html.rs
@@ -8,27 +8,23 @@ use rustc_serialize::json;
 use std::process::Command;
 
 pub fn output(gathered: &BTreeMap<String, ResultStat>) {
-    let mut f = File::open("html/template.html").unwrap();
-    let mut html = String::new();
-    f.read_to_string(&mut html).unwrap();
-
     let json_data = json::encode(&gathered).unwrap();
     let script = format!("var data = {};", json_data);
-    let result: String = html.replace("{architect-data}", &script);
+    let result: String = include_str!("template.html").replace("{architect-data}", &script);
 
-    let mut f = File::create("html/result.html").unwrap();
+    let mut f = File::create("result.html").unwrap();
     f.write_all(result.as_bytes()).unwrap();
 
-    println!("Generated html/result.html, open it in your browser.");
+    println!("Generated result.html, open it in your browser.");
     #[cfg(target_os = "linux")]
     Command::new("xdg-open")
-        .arg("html/result.html")
+        .arg("result.html")
         .output()
         .unwrap_or_else(|e| panic!("failed to open html automatically: {}", e));
 
     #[cfg(target_os = "macos")]
     Command::new("open")
-        .arg("html/result.html")
+        .arg("result.html")
         .output()
         .unwrap_or_else(|e| panic!("failed to open html automatically: {}", e));
 }
