705fee417cdf2dfad1630dbf2c49df5097f4f62d
White-Oak <lpzhelud@gmail.com>
2016-06-21 13:22:53 +0300

    Added code to decode csv dumps

--- src/csv_output.rs
+++ src/csv_output.rs
@@ -2,19 +2,26 @@ use diff::*;
 
 use std::io::prelude::*;
 use std::fs::File;
-use csv;
+use csv::{Writer, Reader, Result};
+use git2::*;
 
 pub fn csv_dump(stats: &[Stat]) {
-    let mut w = csv::Writer::from_memory();
+    let mut w = Writer::from_memory();
 
     for record in stats {
-        w.encode(LocalStat::from_stat(record.clone())).unwrap();
+        w.encode(LocalStat::from(record.clone())).unwrap();
     }
     let mut f = File::create("data.csv").unwrap();
     f.write_all(w.as_bytes()).unwrap();
 }
 
-#[derive(RustcEncodable)]
+pub fn csv_load() -> Result<Vec<Stat>> {
+    let mut r = Reader::from_file("data.csv")?;
+    let ls_vec: Vec<LocalStat>  = r.decode().collect()?;
+    Ok(ls_vec.into_iter().map(|ls| ls.into()).collect())
+}
+
+#[derive(RustcEncodable, RustcDecodable)]
 struct LocalStat {
     pub id: String,
     pub author: String,
@@ -26,8 +33,27 @@ struct LocalStat {
     pub message: String,
 }
 
-impl LocalStat {
-    fn from_stat(stat: Stat) -> Self {
+impl From<LocalStat> for Stat {
+    fn from(stat: LocalStat) -> Self {
+        let message = if stat.message.is_empty() {
+            None
+        } else {
+            Some(stat.message)
+        };
+        Stat {
+            id: Oid::from_str(&stat.id).unwrap(),
+            author: stat.author,
+            email: stat.email,
+            inserts: stat.inserts,
+            dels: stat.dels,
+            time: Time::new(stat.time, stat.offset_minutes),
+            message: message,
+        }
+    }
+}
+
+impl From<Stat> for LocalStat {
+    fn from(stat: Stat) -> Self {
         let message = match stat.message {
             Some(m) => m,
             None => "".to_string(),
